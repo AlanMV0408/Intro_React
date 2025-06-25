aa6fbd9979919d89356084b2b5a4caba52132c11
AlanMV <mv.alan4806@gmail.com>
2025-06-24 21:39:10 -0600

    ⭐ Hooks, localStorage, Components

--- React_Router/src/App.jsx
+++ React_Router/src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
-
+import {useState, useEffect } from 'react';
+import Navbar from "./components/Navbar"
+import AppRoutes from "./routes/AppRoutes"
 function App() {
- 
+
+   const[user, setUser] = useState (null);
+
+   useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('user'));
+    if(userData) setUser(userData)
+   }, [])
 
   return (
     <>
-      
-      <h1>Hola</h1>
-     
-        
+     <Navbar user={user} setUser={setUser} />
+     <AppRoutes user={user} setUser={setUser} />
     </>
   )
 }

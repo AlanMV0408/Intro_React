8ca2c38a2357e00b8442932e93a4a6bfc3f59a5b
AlanMV <mv.alan4806@gmail.com>
2025-06-26 19:47:23 -0600

    ⭐Renderizado condicional

--- React_Router/src/components/Navbar.jsx
+++ React_Router/src/components/Navbar.jsx
@@ -1,19 +1,31 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+// Navbar recibe el usuario actual y la función para actualizarlo como props
+const Navbar = ({user, setUser}) => {
     return (
-        <nav className="bg-blue-600 text-white p-4 flex justify-between">
-            <div className="space-x-4">
-                <Link to="/">Inicio</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Signup</Link>
-            </div>
-            <button>
-                Cerrar Sesión
-            </button>
-        </nav>
+        //contenedor principal de la barre de navagación
+     <nav className="bg-blue-600 text-white p-4 flex justify-between">
+        <div className="space-x-4">
+            <Link to="/">Inicio</Link>
+            {/* solo se muestran estos links si el usuario NO está autenticado  */}
+            {!user && <Link to="/login">Login</Link>}
+            {!user && <Link to="/signup">Registro</Link>}
+        </div>
+          {/* si el usuario está autenticado, muestra botón para cerrar sesión */}
+          {user && (
+            <button
+            onClick={() => {
+                //Elimina al usuario de mi localStorage
+                localStorage.removeItem("user");
+                //Actualiza el estado del usuario a null (sesión cerrada)
+                setUser(null);
+            }}
+            >
+            Cerrar Sesión
+        </button>
+          )}
+     </nav>
     )
-
 }
 
 export default Navbar;
